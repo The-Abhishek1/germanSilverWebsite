8882d301bb8b7450a4e5c61752be91f266ba883c
The-Abhishek1 <idiot63666@gmail.com>
2024-07-27 16:48:05 +0530

    Removed usePath in page.jsx in blogs

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,11 +1,8 @@
-"use client";
 import React from "react";
-import { usePathname } from "next/navigation";
 import All from "@app/(Components)/(Blogs)/(Sub)/All";
 
 //main function
 function page() {
-  const pathname = usePathname();
   return (
     <>
       <All />
