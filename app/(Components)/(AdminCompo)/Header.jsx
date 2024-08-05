d6b0e21ec8a315498e0408bddf184d9bd8a580c3
The-Abhishek1 <idiot63666@gmail.com>
2024-08-05 10:09:26 +0530

    Added Admin Products Page

--- app/(Components)/(AdminCompo)/Header.jsx
+++ app/(Components)/(AdminCompo)/Header.jsx
@@ -3,48 +3,94 @@ import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
-import { IoMdNotificationsOutline } from "react-icons/io";
+import {
+  IoMdNotificationsOutline,
+  IoIosCloseCircleOutline,
+} from "react-icons/io";
 import SearchIcon from "@mui/icons-material/Search";
-import Sidebar from "./Sidebar";
 import { TiMessage } from "react-icons/ti";
+import ItemsForm from "./ItemsForm";
 //Main function
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const [products, setProducts] = useState(false);
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  const currentDate = date + "-" + month + "-" + year;
   return (
-    <div className="w-full h-[100%] ">
-      <div className="grid grid-flow-col py-3 bg-lime-100 gap-3 place-content-center justify-between w-full grid-cols-7">
-        <div className="pl-[10px] cursor-pointer">
-          <MenuIcon
+    <div className="">
+      <div className=" bg-sky-50 mmd:flex items-center justify-between relative p-4">
+        {showMenu ? (
+          <div
+            className="md:hidden cursor-pointer"
+            onClick={() => {
+              setShowMenu(false);
+            }}
+          >
+            <IoIosCloseCircleOutline size={25} />
+          </div>
+        ) : (
+          <div
+            className="md:hidden cursor-pointer"
             onClick={() => {
               setShowMenu(true);
             }}
-            sx={{ fontSize: 23 }}
-          />
-        </div>
-        <div className="flex relative col-span-5 items-center">
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="Search here"
-            className="w-full outline-none text-[12px] bg-gray-100 rounded-2xl border-none px-5 py-[5px]"
-          />
-          <SearchIcon
-            className="absolute right-5 cursor-pointer"
-            sx={{ fontSize: 20, color: "gray" }}
-          />
-        </div>
-        <div className="flex gap-4 items-center justify-center cursor-pointer ">
-          <IoMdNotificationsOutline size={23} color="#eb8034" />
-          <TiMessage size={23} color="#eb8034" />
-          <AccountCircleIcon sx={{ fontSize: 25, color: "#eb8034" }} />
+          >
+            <MenuIcon />
+          </div>
+        )}
+
+        <h1 className="ms:text-[1rem] uppercase text-center text-[1.5rem] font-bold font-serif ">
+          Welocome Admin
+        </h1>
+        <p className="md:absolute msmmm:text-[13px] font-bold font-serif  top-6 right-14">
+          {currentDate}
+        </p>
+        <div className="mmd:hidden mt-4 flex relative font-bold font-serif flex-col items-center gap-4 w-full">
+          <div className="flex p-3 items-center w-full justify-between bg-white">
+            <div
+              onClick={() => {
+                setProducts(true);
+                setShowMenu(false);
+              }}
+              className="cursor-pointer"
+            >
+              Products
+            </div>
+            <div className="cursor-pointer">Orders</div>
+            <div className="cursor-pointer">Statistics</div>
+            <div className="cursor-pointer">Reviews</div>
+            <div className="cursor-pointer">Customer</div>
+            <div className="cursor-pointer">Transactions</div>
+          </div>
         </div>
       </div>
       {showMenu ? (
-        <div>
-          <Sidebar />
+        <div className="md:hidden absolute bg-slate-100 z-10 p-3 flex flex-col gap-3 mt-2 ml-2 font-serif w-[130px]">
+          <div
+            onClick={() => {
+              setProducts(true);
+              setShowMenu(false);
+            }}
+            className="cursor-pointer"
+          >
+            Products
+          </div>
+          <hr />
+          <div className="cursor-pointer">Orders</div>
+          <hr />
+          <div className="cursor-pointer">Statistics</div>
+          <hr />
+          <div className="cursor-pointer">Reviews</div>
+          <hr />
+          <div className="cursor-pointer">Customer</div>
+          <hr />
+          <div className="cursor-pointer">Transactions</div>
         </div>
       ) : null}
+      {products ? <ItemsForm /> : null}
     </div>
   );
 }
--- app/(Components)/(AdminCompo)/ItemsForm.jsx
+++ app/(Components)/(AdminCompo)/ItemsForm.jsx
@@ -4,11 +4,14 @@ import { db, storage, auth } from "@app/(Firebase)/config";
 import { getStorage, ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { FormEvent } from "react";
 import { addDoc, collection } from "firebase/firestore";
-
+import { item } from "./Item";
+import Image from "next/image";
 //Main Function
 function ItemsForm() {
   //State For Image
   const [imageUpload, setImageUpload] = useState(null);
+  const [products, setProducts] = useState(false);
+  const [newProducts, setNewProducts] = useState(true);
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -42,44 +45,109 @@ function ItemsForm() {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-2 bg-slate-200"
-      >
-        <h1>collections form</h1>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="p-2.5 outline-none text-[14px] rounded-md w-full"
-          />
+    <div className="p-4 flex relative flex-col w-full items-center gap-6 font-serif">
+      <div className="flex items-center gap-6">
+        <button
+          onClick={() => {
+            setNewProducts(true);
+            setProducts(false);
+          }}
+          className="bg-indigo-950 rounded-md p-1 text-white px-3"
+        >
+          Add Product
+        </button>
+        <button
+          onClick={() => {
+            setNewProducts(false);
+            setProducts(true);
+          }}
+          className="bg-indigo-950 rounded-md p-1 text-white px-3"
+        >
+          List Products
+        </button>
+      </div>
+      {newProducts ? (
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-full mss:w-[70%] flex-col gap-2 px-5"
+        >
+          <h1 className="text-center font-bold">
+            Enter The Details of New Product
+          </h1>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="p-2 bg-slate-50 border-orange-500 border-[1px] outline-none text-[14px] rounded-md w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="type">Item Type</label>
+            <input
+              type="text"
+              className="p-2 bg-slate-50 border-orange-500 border-[1px] outline-none text-[14px] rounded-md w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="actualValue">Actual Value</label>
+            <input
+              type="text"
+              className="p-2 bg-slate-50 border-orange-500 border-[1px] outline-none text-[14px] rounded-md w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="discountValue">Discount Value</label>
+            <input
+              type="text"
+              className="p-2 bg-slate-50 border-orange-500 border-[1px] outline-none text-[14px] rounded-md w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="image">Upload the Image</label>
+            <input
+              type="file"
+              onChange={(event) => {
+                setImageUpload(event.target.files[0]);
+              }}
+              className="p-2 bg-slate-50 border-orange-500 border-[1px] outline-none text-[14px] rounded-md w-full"
+            />
+          </div>
+          <div className="flex items-center justify-center mt-5 w-full">
+            <button
+              className="bg-indigo-950 w-full rounded-md p-[6px] text-white px-3"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      ) : null}
+      {products ? (
+        <div className=" flex flex-row justify-center flex-wrap gap-5 items-center">
+          {item.map((i) => {
+            return (
+              <div
+                key={i.id}
+                className="flex bg-slate-100 w-[170px] h-[250px] justify-center px-2 flex-col gap-1 items-center"
+              >
+                <Image
+                  src={i.image}
+                  alt={i.text}
+                  className="w-[100px] h-[100px] "
+                />
+                <h3 className="text-[14px]">{i.text}</h3>
+                <h5 className="text-[14px]">{i.typeOf}</h5>
+                <h5 className="text-[14px]">{i.ActualPrice}</h5>
+
+                <button className="bg-indigo-950 w-full rounded-md p-[6px] text-white px-3">
+                  Delete
+                </button>
+              </div>
+            );
+          })}
         </div>
-        <div>
-          <label htmlFor="type">Item Type</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="actualValue">Actual Value</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="discountValue">Discount Value</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="image">Upload the Image</label>
-          <input
-            type="file"
-            onChange={(event) => {
-              setImageUpload(event.target.files[0]);
-            }}
-          />
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      ) : null}
+      <div className="bg-sky-100 z-[-2] top-0 blur-3xl h-full absolute w-full"></div>
     </div>
   );
 }
