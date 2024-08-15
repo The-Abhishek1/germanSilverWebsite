dba6a8f7243b260be54a1564fc4102d5e48b6715
The-Abhishek1 <idiot63666@gmail.com>
2024-08-16 00:57:06 +0530

    Nothing

---
+++ app/idiot/page.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="font-serif pb-32 p-10 flex flex-col items-center gap-4">
+      <h1 className="font-bold text-[22px]">Hii, Good Morning😶‍🌫️</h1>
+      <div className="flex gap-2">
+        <p className="text-red-600 inline-block font-bold">Caution:</p>
+        <p>Please read it carefully</p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p>
+          I'm Sorry for the Disturbance, I don't know why but I felt like
+          proposing you today. Currently it's 12:37 AM Mid-Night I'm trying to
+          build a new website but don't know why it's not working. So I created
+          a extra path in my old website🤦‍♂️. Once I think about something I just
+          don't care I'll just get it done. I hate giving up and I don't want to
+          regret later about thinking that I should have done that.
+        </p>
+        <p>
+          It's been more than one year I proposed you for the first time. It was
+          nice experience though. I think I have proposed you more than 5 times
+          but got rejected everytime😂😂 it's ok I don't mind failing. Atleast
+          it's still way more better than not trying. Don't know what's your
+          mood and life status now but still doing all these things like an
+          Idiot.🙊
+        </p>
+        <p>
+          Some says if you don't go after what you want, you'll never get it.
+          But some says if it really your's it'll definitely come back. Don't
+          know whom to believe, but I really enjoy experiencing the life rather
+          than escaping from it.
+        </p>
+        <p>
+          I think I'm an Idiotic Idiot, 2 day's back when I said "Just get marry
+          idiot" to you, you replied "You won't ahh", I don't know am I
+          tubelight or what, but still I didn't understood what you said🤦‍♂️😂😂
+        </p>
+        <p>
+          I really don't know what to type😂😂, never felt learning programming
+          and computer difficult but understanding a women is way more
+          complicated than nuclear bomb💣 and it really need guts to propose a
+          girl even after being rejected.
+        </p>
+        <p>
+          You are the one running in my mind 24 x 7. I still can't forget the
+          days, I sat next to you and the day I heard your voice on call for 5s
+          on September 15th last year. There are only two things I learnt from
+          love
+        </p>
+        <div>
+          <p>1. You may gonna lose yourself, while trying to win someone.</p>
+          <p>2. You'll never get to know the way she wanted to be loved.</p>
+        </div>
+        <p>
+          Please ignore chat with us popup on bottom right corner it's my
+          project
+        </p>
+        <p>
+          Tomorrow I'll get ready to receive scoldings and rejections from you,
+          sounds amazing maa😂😂 But atleast give me a remarks/feedback for this
+          please I'm gonna correct it
+        </p>
+        <h1>lastly,</h1>
+        <h1 className="font-bold text-[20px]">
+          “The moon is beautiful, isn't it?”
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default page;
